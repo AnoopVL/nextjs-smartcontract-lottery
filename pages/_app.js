8300cd74a0c04233e4ad22575e5903e309e27afb
AnoopVL <avlanjekar4@gmail.com>
2023-03-22 00:32:47 +0530

    NotificationProvider added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
 export default function App({ Component, pageProps }) {
   return (
     // initializeOnMount is the optionality to jump into servers to add some more features to our website
     <MoralisProvider initializeOnMount={false}>
-      <Component {...pageProps} />
+      <NotificationProvider>
+        <Component {...pageProps} />
+      </NotificationProvider>
     </MoralisProvider>
   )
 }
